ed3312b50c82fb2e908b38b2a85a6dc5f71215d1
Ddayisme <122437344+Ddayisme@users.noreply.github.com>
2023-01-27 13:31:21 +0100

    Initial commit

---
+++ src/main/java/com/example/assignment3_noroff_janfeb/Assignment3NoroffJanFebApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment3_noroff_janfeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3NoroffJanFebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment3NoroffJanFebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/assignment3_noroff_janfeb/Assignment3NoroffJanFebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment3_noroff_janfeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3NoroffJanFebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
